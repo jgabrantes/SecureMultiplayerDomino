ce0b714c96f4fdf36b1107ce0d3bfe8eba61d16c
jgabrantes <jgabrantes@ua.pt>
2021-01-30 20:00:31 +0000

    fix

--- game_server-client/server.py
+++ game_server-client/server.py
@@ -139,7 +139,7 @@ class Server:
         print('Pseudonymized stock sent to',client)
 
     def recieveShuf1(self, client):
-        cipherText = self.conn[client].recv(16000)
+        cipherText = self.conn[client].recv(16384)
         plainText = security.aesDecrypt(cipherText, self.sessionKey[client])
         message = pickle.loads(plainText)
 
